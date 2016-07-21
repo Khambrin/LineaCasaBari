4443c282db96e504f5433ccf798048d897497b65
Alice Black <blackfluid44@gmail.com>
2016-07-21 15:29:51 +0200

    Javascript e Perl  controlli aggiunta prodotto in gestione sito

---
+++ public_html/js/add_product.js
@@ -0,0 +1,19 @@
+
+function prForm() {//form aggiunta prodotto
+
+
+
+	var prezzo1  = productform.prezzo.value;
+
+
+
+	var rgprezzo = /^[0-9]*\,[0-9]{2,2}$/ ;
+	var dprezzo = rgprezzo.test(prezzo1);
+	if(dprezzo==false) {
+		document.getElementById("formatoprezzo").innerHTML = "Inserisci un prezzo valido, ad esempio 1,00";
+        	return false;
+	}
+
+
+    	return true;
+}
