5cef876d0757cdcd0a8e5d7f963c94464e56c704
Alice Black <blackfluid44@gmail.com>
2016-07-24 12:42:52 +0200

    Aggiustati controlli modify_ac.js

--- public_html/js/modify_ac.js
+++ public_html/js/modify_ac.js
@@ -5,29 +5,34 @@
 			var sem  = modacform.nuova_email.value;
     			var n = str.length;
 			var t = snt.length;
-    			if(n<8) {
-				document.getElementById("shortpassword").innerHTML = "Nuova Password troppo corta, minimo 8 caratteri";
-				document.getElementById("longpassword").innerHTML = "";
-        			return false;
-	
-    			}
-			else if(n>20) {
-				document.getElementById("shortpassword").innerHTML = "";
-				document.getElementById("longpassword").innerHTML = "Nuova Password troppo lunga, massimo 20 caratteri";
-        			return false;
-    			}
-			if(t<8 | t>15) { //ITU-T recommendation E.164
-				document.getElementById("ernumber").innerHTML = "Numero non valido";
-        			return false;
-	
-    			}
-			var rgx = /^\+?([0-9]){8,15}$/;
-    			var scr = rgx.test(snt);
-			if(scr==false){
-				document.getElementById("ernumber").innerHTML = "Numero non valido, uso di caratteri proibiti"; 
-				return false;
+			if(str)
+			{
+    				if(n<8) {
+					document.getElementById("shortpassword").innerHTML = "Nuova Password troppo corta, minimo 8 caratteri";
+					document.getElementById("longpassword").innerHTML = "";
+        				return false;
+		
+    				}
+				else if(n>20) {
+					document.getElementById("shortpassword").innerHTML = "";
+					document.getElementById("longpassword").innerHTML = "Nuova Password troppo lunga, massimo 20 caratteri";
+        				return false;
+    				}
 			}
-    			
+			if(snt)
+			{
+				if(t<8 | t>15) { //ITU-T recommendation E.164
+					document.getElementById("ernumber").innerHTML = "Numero non valido";
+        				return false;
+		
+    				}
+				var rgx = /^\+?([0-9]){8,15}$/;
+    				var scr = rgx.test(snt);
+				if(scr==false){
+					document.getElementById("ernumber").innerHTML = "Numero non valido, uso di caratteri proibiti"; 
+					return false;
+				}
+    			}
 			
 			var rgem = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ ;
 			var gem = rgem.test(sem);
