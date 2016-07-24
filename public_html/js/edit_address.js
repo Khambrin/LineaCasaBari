79251d58db5b30ed7d02324631305f1b15e438d8
Alice Black <blackfluid44@gmail.com>
2016-07-24 14:35:50 +0200

    Newsletter corretta in tutti i temp

--- public_html/js/edit_address.js
+++ public_html/js/edit_address.js
@@ -1,17 +1,19 @@
 
-function editForm() {// aggiungi indirizzo
+function editForm() {// modifica indirizzo
 
 
-
-
-
+	var via1  = editaddressform.via.value;
+	var num1  = editaddressform.numero.value;
+	var cit1  = editaddressform.citta.value;
 	var pro1  = editaddressform.provincia.value;//2 letters
 	var cap1  = editaddressform.cap.value;//5 digits
 
     	var lpro1 = pro1.length;
 	var lcap1 = cap1.length;
 			
-
+	if(via1==false){ document.getElementById("editvia").innerHTML = "Inserisci una via"; return false;}
+	if(num1==false){ document.getElementById("editnum").innerHTML = "Inserisci un numero civico"; return false;}
+	if(cit1==false){ document.getElementById("editcit").innerHTML = "Inserisci una città"; return false;}
 
 	var rgprov = /^([A-Za-z]){2,2}$/ ;
 	var dprov = rgprov.test(pro1);
