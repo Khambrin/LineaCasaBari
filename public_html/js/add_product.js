25079caaf54f2816adb0de7c8a36b82a6dee9629
Alice Black <blackfluid44@gmail.com>
2016-07-27 14:49:26 +0200

    Corretto js aggiungi prodotto

--- public_html/js/add_product.js
+++ public_html/js/add_product.js
@@ -10,7 +10,7 @@ function prForm() {//form aggiunta prodotto
 	var rgprezzo = /^[0-9]*\,[0-9]{2,2}$/ ;
 	var dprezzo = rgprezzo.test(prezzo1);
 	if(dprezzo==false) {
-		document.getElementById("formatoprezzo").innerHTML = "Inserisci un prezzo valido, ad esempio 1,00";
+		document.getElementById("formatoprezzo").innerHTML = "Inserisci un prezzo valido, ad esempio 1,00 oppure 12";
         	return false;
 	}
 
