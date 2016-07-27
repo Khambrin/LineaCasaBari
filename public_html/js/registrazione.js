2b96972246010ccfaf25622c9f9cce75eac530b5
Alice Black <blackfluid44@gmail.com>
2016-07-27 17:36:18 +0200

    Aggiustamento registrazione js

--- public_html/js/registrazione.js
+++ public_html/js/registrazione.js
@@ -5,29 +5,44 @@ function validateForm() {
 	var cog  = recform.cognome.value;
     	var ema  = recform.email.value;
 	var str  = recform.password.value;
+	var cfp  = recform.conf_password.value;
 
-	if(nom==false){ document.getElementById("nom").innerHTML = "Completare il campo Nome"; return false;}
-	if(cog==false){ document.getElementById("cog").innerHTML = "Completare il campo Cognome"; return false;}
-	if(ema==false){ document.getElementById("ema").innerHTML = "Completare il campo Email"; return false;}
+	if(nom==false){ document.getElementById("nom").innerHTML = "Completare il campo Nome"; }
+	else{document.getElementById("nom").innerHTML = "";}
+	if(cog==false){ document.getElementById("cog").innerHTML = "Completare il campo Cognome"; }
+	else{document.getElementById("cog").innerHTML = "";}
+	if(ema==false){ document.getElementById("ema").innerHTML = "Completare il campo Email"; }
+	else{document.getElementById("ema").innerHTML = "";}
 
-	var rgxstr = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ ;
-	var gem = rgem.test(str);
+	var rgxstr = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ ; 
+	var gem = rgxstr.test(ema);
 	if(gem==false){
-		document.getElementById("emai").innerHTML = "Email non valida"; 
-		return false;
+		document.getElementById("emailrec").innerHTML = "Email non valida"; 
+		//return false;
 	}
+	else{document.getElementById("emailrec").innerHTML = "";}
+	
+	if(str==false){ document.getElementById("cem").innerHTML = "Completare il campo Password"; }
+	else{document.getElementById("cem").innerHTML = "";}
+
+	
 
 	var n = str.length;
+	
     	if(n<8) {
 		document.getElementById("shortpassword").innerHTML = "Password troppo corta, minimo 8 caratteri";
-		document.getElementById("longpassword").innerHTML = "";
-        	return false;
+		//document.getElementById("longpassword").innerHTML = "";
+        	//return false;
     	}
-	else if(n>20) {
-		document.getElementById("shortpassword").innerHTML = "";
+	else{ document.getElementById("shortpassword").innerHTML = "";}
+	if(n>20) {
+		//document.getElementById("shortpassword").innerHTML = "";
 		document.getElementById("longpassword").innerHTML = "Password troppo lunga, massimo 20 caratteri";
-        	return false;
+        	//return false;
     	}
+	else{ document.getElementById("longpassword").innerHTML = "";}
+	
+	if(nom==false|cog==false|ema==false|gem==false|str==false|n<8|n>20){return false;}
     	
 	return true;
 }
