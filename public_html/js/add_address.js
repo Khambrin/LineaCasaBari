3768a3eabe6cd4520e20da247eb265fe21053a2f
Alice Black <blackfluid44@gmail.com>
2016-07-27 17:47:24 +0200

    Aggiustati controlli js aggiungi indirizzo

--- public_html/js/add_address.js
+++ public_html/js/add_address.js
@@ -7,19 +7,21 @@ function addForm() {// aggiungi indirizzo
 	var pro1  = addressform.provincia.value;//2 letters
 	var cap1  = addressform.cap.value;//5 digits
 
-    	var lpro1 = pro1.length;
-	var lcap1 = cap1.length;
 			
-	if(via1==false){ document.getElementById("addvia").innerHTML = "Inserisci una via"; return false;}
-	if(num1==false){ document.getElementById("addnum").innerHTML = "Inserisci un numero civico"; return false;}
-	if(cit1==false){ document.getElementById("addcit").innerHTML = "Inserisci una città"; return false;}
+	if(via1==false){ document.getElementById("addvia").innerHTML = "Inserisci una via"; }
+	else{document.getElementById("addvia").innerHTML = "";}
+	if(num1==false){ document.getElementById("addnum").innerHTML = "Inserisci un numero civico"; }
+	else{document.getElementById("addnum").innerHTML = "";}
+	if(cit1==false){ document.getElementById("addcit").innerHTML = "Inserisci una città"; }
+	else{document.getElementById("addcit").innerHTML = ""; }
 
 	var rgprov = /^([A-Za-z]){2,2}$/ ;
 	var dprov = rgprov.test(pro1);
 	if(dprov==false) {
 		document.getElementById("addtwoprov").innerHTML = "Inserisci una provincia valida, ad esempio PD";
-        	return false;
+        	//return false;
 	}
+	else{document.getElementById("addtwoprov").innerHTML = "";}
 
   
 			
@@ -27,7 +29,9 @@ function addForm() {// aggiungi indirizzo
 	var dcap = rgcap.test(cap1);
 	if(dcap==false) {
 		document.getElementById("adddigitscap").innerHTML = "Inserisci un CAP valido";
-        	return false;
+        	//return false;
 	}
+	else{document.getElementById("adddigitscap").innerHTML = "";}
+	if(via1==false|num1==false|cit1==false|dprov==false|dcap==false){return false;}
     	return true;
 }
